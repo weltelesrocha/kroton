3aa0ac95ca78148311519c7e992602ac35ee590e
Wellington Rocha <wellington.rocha@atlasproj.com>
2019-12-20 13:20:43 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const n = 5;
+const v = [4, -3, 7, 8, 2];
+const validateNumberN = (number) => {
+    if (!Number.isInteger(number)
+        || number < 2
+        || number > 10) {
+        throw new Error(`Invalid number N ${n}`);
+    }
+}
+const validateNumberV = (number) => {
+    if (!Number.isInteger(number)
+        || number > 1000000000
+        || number < -1000000000) {
+        throw new Error(`Invalid number V ${number}`);
+    }
+}
+const validateArrayNumberV = (numberArray) => {
+    numberArray.map(number => validateNumberV(number));
+};
+const lowestPossibleSum = (n, v) => {
+    return v
+        .sort()
+        .slice(0, n - 1)
+        .reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+const highestPossibleSum = (n, v) => {
+    return (v
+        .sort()
+        .reverse())
+        .slice(0, n - 1)
+        .reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+const calculateAlgorithm = (n, v) => {
+    try {
+        validateNumberN(n);
+        validateArrayNumberV(v);
+        return [
+            lowestPossibleSum(n, v),
+            highestPossibleSum(n, v)
+        ];
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+console.log(calculateAlgorithm(n, v));
+
